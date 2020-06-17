94339e511159fe36bf6c6602588ab0f56e7d5b12
Samrood Ali <devsamrood@gmail.com>
2020-06-17 21:15:58 +0400

    Add:Project dependencies

--- commentsOnlyJS/server.js
+++ commentsOnlyJS/server.js
@@ -1,14 +1,16 @@
 // Setup empty JS object to act as endpoint for all routes
-// Express to run server and routes
-
+projectData = {}
+    // Express to run server and routes
+const express = require('express');
 // Start up an instance of app
-
+const app = express();
 /* Dependencies */
+const bodyParser = require('body-parser');
 /* Middleware*/
-
+const cors = require('cors');
 //Here we are configuring express to use body-parser as middle-ware.
 // Cors for cross origin allowance
-
+app.use(cors());
 // Initialize the main project folder
 
 // Spin up the server
@@ -19,4 +21,3 @@
 // Callback function to complete GET '/all'
 
 // Post Route
-  
