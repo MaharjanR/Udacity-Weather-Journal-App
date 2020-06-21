048e526dfb81a8908eba9458bbe22bbfe962e145
Samrood Ali <devsamrood@gmail.com>
2020-06-21 13:34:18 +0400

    Add:Dynamic Api call on OpenWeatherApp

--- website/app.js
+++ website/app.js
@@ -1,6 +1,6 @@
 /* Global Variables */
 const apiKey = '28465ad65f0da40dc1008729930cf40a';
-const baseUrl = 'https://api.openweathermap.org/data/2.5/weather?zip=676101,in&appid=';
+const baseUrl = 'https://api.openweathermap.org/data/2.5/weather?zip=';
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
@@ -11,9 +11,14 @@ const zipText = document.getElementById("zip");
 const feelingsText = document.getElementById('feelings');
 
 //getting weatherinfo - using a fetch call to openWeatherMap
-const weatherInfo = async zip => await fetch(baseUrl + apiKey);
+const weatherInfo = async zip => await fetch(`${baseUrl}${zip}&appid=${apiKey}`);
 
-
-submitBtn.addEventListener('change', async() => {
+submitBtn.addEventListener('click', async() => {
+    submitBtn.textContent = 'Generating...';
     const zip = zipText.value;
+    const feelings = feelingsText.value;
+    const response = await weatherInfo(zip);
+    console.log(response.json())
+    submitBtn.textContent = 'Generate';
+
 })
